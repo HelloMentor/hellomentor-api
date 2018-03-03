62ff351f72217f3543c57cdae1ff08ed529148a0
Ben Inada <me@beninada.com>
2018-03-03 00:02:08 -0800

    toAuthJson() returns all user info.

--- database/models/user.js
+++ database/models/user.js
@@ -48,9 +48,23 @@ UserSchema.methods.generateJWT = function() {
 
 UserSchema.methods.toAuthJSON = function(){
   return {
+    token: this.generateJWT(),
     id: this._id,
     email: this.email,
-    token: this.generateJWT()
+    role: this.role,
+    f_name: this.f_name,
+    l_name: this.l_name,
+    headline: this.headline,
+    summary: this.summary,
+    city: this.city,
+    country: this.country,
+    linkedin_u_name: this.linkedin_u_name,
+    skills: this.skills,
+    gender: this.gender,
+    dob: this.dob,
+    wanted_skills: this.wanted_skills,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt
   };
 };
 
