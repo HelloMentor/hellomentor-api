5321454954e38ca168180b8508d732009f6c6e5e
Ben Inada <me@beninada.com>
2018-03-14 01:21:27 -0700

    Require auth and send back email as username.

--- routes/chat.js
+++ routes/chat.js
@@ -2,22 +2,27 @@ var express = require('express');
 var router = express.Router();
 var Twilio = require('twilio');
 var Chance = require('chance');
+var auth = require('./auth');
+var User = require('../database/models/user');
 
 var AccessToken = Twilio.jwt.AccessToken;
 var ChatGrant = AccessToken.ChatGrant;
 var chance = new Chance();
 
-router.get('/token', function(req, res) {
+router.get('/token', auth.required, function(req, res) {
   const token = new AccessToken(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_API_KEY, process.env.TWILIO_API_SECRET);
 
-  token.identity = chance.name();
-  token.addGrant(new ChatGrant({
-    serviceSid: process.env.TWILIO_CHAT_SERVICE_SID
-  }));
+  User.findById(req.payload.id).then(function(user) {
+    token.identity = user.email;
 
-  res.send({
-    identity: token.identity,
-    jwt: token.toJwt()
+    token.addGrant(new ChatGrant({
+      serviceSid: process.env.TWILIO_CHAT_SERVICE_SID
+    }));
+
+    res.send({
+      identity: token.identity,
+      jwt: token.toJwt()
+    });
   });
 });
 
