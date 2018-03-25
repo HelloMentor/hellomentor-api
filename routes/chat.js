a862f308e51cbda143d58b7ac6fae742eb3f1898
Ben Inada <me@beninada.com>
2018-03-24 21:17:33 -0700

    Implement sendgrid email notification on conversation start.

--- routes/chat.js
+++ routes/chat.js
@@ -4,6 +4,7 @@ var auth = require('./auth');
 var User = require('../database/models/user');
 var Channel = require('../database/models/channel');
 var Message = require('../database/models/message');
+var sgMail = require('@sendgrid/mail');
 
 router.get('/channels', auth.required, function(req, res) {
   var liuId = req.query.user_id;
@@ -47,9 +48,33 @@ router.post('/channels', auth.required, function(req, res) {
     User.find({
       _id: { $in: memberIds }
     }).then(function(members) {
-      for (var i = 0; i < members.length; i++) {
-        members[i].channels.push(channel);
-        members[i].save();
+      // Set up sendgrid mail
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      sgMail.setSubstitutionWrappers('<%', '%>');
+      let creator = members.find(m => m._id.toString() === channel.creator.toString());
+      let subject = creator.f_name + ' started a conversation with you'
+
+      // Save the channel to each member
+      for (let i = 0; i < members.length; i++) {
+        let member = members[i];
+        member.channels.push(channel);
+        member.save();
+
+        // Send an email notification to the invitee if a direct msg
+        if (channel.type === 'direct' && (member._id !== creator._id)) {
+          sgMail.send({
+            to: member.email,
+            from: 'donotreply@hellomentor.io',
+            subject: subject,
+            templateId: 'b84bc07a-26bc-42f7-82ac-5ea82f2b0125',
+            substitutions: {
+              recipient_f_name: member.f_name,
+              sender_f_name: creator.f_name,
+              sender_l_name: creator.l_name,
+              subject: subject
+            }
+          });
+        }
       }
 
       return res.json({ channel: channel });
